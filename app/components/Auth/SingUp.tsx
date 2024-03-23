a66c1750e72ab699134319b2cc75828b02349e21
Your Name <you@example.com>
2024-03-23 22:45:36 +0600

    frist-commit

--- app/components/Auth/SingUp.tsx
+++ app/components/Auth/SingUp.tsx
@@ -8,9 +8,9 @@ const SingUp = () => {
   return (
     <div className='overflow-hidden h-[84vh] flex flex-col items-center justify-center '>
        {/* iamge */}
-       <Image src={ImageUrl} alt='' width={400} height={200} />
+       <Image src={ImageUrl} alt='image' width={400} height={200} />
        {/* singup button */}
-       <button onClick={() => {signIn('google', { callbackUrl: process.env.NEXTAUTH_PUBLIC_URL })}}className="px-12 py-3 mt-[2rem] bg-purple-700 hover:bg-purple-900 transition-all duration-300 rounded-lg text-white">
+       <button onClick={() => {signIn('google', {callbackUrl:"/"})}}className="px-12 py-3 mt-[2rem] bg-purple-700 hover:bg-purple-900 transition-all duration-300 rounded-lg text-white">
         Sing Up Now
        </button>
         
--- app/components/Helper/User.tsx
+++ app/components/Helper/User.tsx
@@ -1,15 +1,16 @@
 'use client';
 import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
 
 interface Props {
     session: Session;
 }
 const User = ({session}:Props) => {
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer" onClick={()=>{signOut({callbackUrl:'/signup'})}}>
         <img src={`${session?.user?.image}`} alt="user" className="w-[50px] h-[50px] rounded-full" />
     </div>
   )
 }
 
-export default User
+export default User;
